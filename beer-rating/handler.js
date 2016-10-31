35c5e141ea88e925542e1de5da93e23077a63d30
snese <hclo@snese.net>
2016-10-31 14:52:02 +0800

    addRating

--- beer-rating/handler.js
+++ beer-rating/handler.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -14,3 +17,26 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.addRating = (event, context, callback) => {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var params = JSON.parse(event.body);
+  var Item = {
+      id: uuid.v4(),
+      beer: params.beer,
+      rating: Number(params.rating)
+  };
+
+  docClient.put({TableName: 'slsbeer', Item: Item}, (error) => {
+    if (error) {
+      callback(error);
+    }
+
+    callback(null, {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+    });
+  });
+}
